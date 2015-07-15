75a65db950495d29ef7d864a8a9305b64f05a3b6
ubuntu-1404 <simul2013@126.com>
2015-07-15 10:34:21 +0800

    Update bakup.sh
    update backup script

--- bakup.sh
+++ bakup.sh
@@ -1,5 +1,5 @@
 timep=`date -d today +"%Y-%m-%d_%H:%M:%S"`
-backupath=/home/ubuntu/${timep}
-tarFile=/home/ubuntu/startAgent.sh
-mkdir ${backupath}
-cp -R ${tarFile} ${backupath}
+mkdir /backup/${timep}
+tar -zcvf /backup/${timep}/backup.tar.gz /mail/linkgent.com/
+timed=`date -d today +"%Y-%m-%d_%H:%M:%S"`
+echo "startTime=${timep} and endTime=${timed}" >> /backup/${timep}/last
