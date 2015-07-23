102126a529d473685fdd96a770605e93625675bd
ubuntu-1404 <simul2013@126.com>
2015-07-23 14:35:25 +0800

    Update bind9.sh
    bind-2015-7-23

--- bind9.sh
+++ bind9.sh
@@ -1,15 +1,15 @@
 namehost=dns.wodezoon.com
 apt-get install bind9
 hostname ${namehost}
-#echo "forwarders{" >> /etc/bind/named.conf.options
-#echo "	219.141.136.10" >> /etc/bind/named.conf.options
-#echo "};" >> /etc/bind/named.conf.options
+sed -i "/forwarders {/,+0a forwarders {" /home/ubuntu/demo.txt
+sed -i "/0.0.0.0/,+0a 219.141.136.10" /home/ubuntu/demo.txt
+sed -i "/219.141.136.10/,+0a };" /home/ubuntu/demo.txt
 
 cp /etc/bind/named.conf.default-zones /etc/bind/named.conf.default-zones.bak
 #nametoip in default
 echo "zone \"wodezoon.com\"  {" >> /etc/bind/named.conf.default-zones
-echo "	type master;" >> /etc/bind/named.conf.default-zones
-echo "	file \"/etc/bind/nametoip\";" >> /etc/bind/named.conf.default-zones
+echo "  type master;" >> /etc/bind/named.conf.default-zones
+echo "  file \"/etc/bind/nametoip\";" >> /etc/bind/named.conf.default-zones
 echo "};" >> /etc/bind/named.conf.default-zones
 
 #iptoname in default
@@ -20,10 +20,12 @@ echo "};" >> /etc/bind/named.conf.default-zones
 
 #update nametoip
 cp -r /etc/bind/db.local /etc/bind/nametoip
-
-sed -i "SOA/c@       IN      SOA     wodezoon.com. post.wodezoon. (" /etc/bind/nametoip
-sed -i "NS/c@       IN      NS      wodezoon.com." /etc/bind/nametoip
-sed -i "AAAA/cmaster       IN      A    192.168.1.101" /etc/bind/nametoip
+sed -i "/localhost. root.localhost. (/,+0a @       IN      SOA     wodezoon.com. root.localhost. (" /etc/bind/nametoip
+sed -i "/localhost. root.localhost. (/,+0d" /etc/bind/nametoip
+sed -i "/NS/,+0d" /etc/bind/nametoip
+sed -i "/127.0.0.1/,+0i @       IN      NS      wodezoon.com." /etc/bind/nametoip
+sed -i "/AAAA/,+0a manager IN      A       192.168.102.253" /etc/bind/nametoip
+sed -i "/AAAA/,+0d" /etc/bind/nametoip
 
 #update iptoname
 #cp -r /etc/bind/db.127 /etc/bind/iptoname
