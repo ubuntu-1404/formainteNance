15b7cb9a7d9b320d03e035be34b034b6d6d6c803
ubuntu-1404 <simul2013@126.com>
2015-11-05 13:13:28 +0800

    Update bakup.sh
    how to find 、compare and backup particular files

--- bakup.sh
+++ bakup.sh
@@ -1,5 +1,30 @@
+#!/bin/bash
 timep=`date -d today +"%Y-%m-%d_%H:%M:%S"`
-mkdir /backup/${timep}
-tar -zcvf /backup/${timep}/backup.tar.gz /mail/linkgent.com/
-timed=`date -d today +"%Y-%m-%d_%H:%M:%S"`
-echo "startTime=${timep} and endTime=${timed}" >> /backup/${timep}/last
+goal=("*.xml" "*.conf")
+goalpath="/root/aback"
+backdir="/backup/${timep}"
+
+#create backup dirary
+if [ ! -d ${backdir} ] ; then
+ mkdir ${backdir}
+fi
+#find goals and backup them (include compare oldbackups and new one)
+for goals in "${goal}" ; do
+ dirs=(`find ${backdir} -name "${goals}"`)
+ for i in "${dirs[@]}";do
+  if [ ! -f ${goalpath}/${i##*/} ] ; then
+   cp ${i} ${goalpath}/${i##*/}
+  else
+   type=(`diff -q ${goalpath}/${i##*/} ${i}`)
+   if [ ${type[4]} =  "differ" ] ;then
+    mv ${goalpath}/${i##*/} ${goalpath}/${i##*/}.bak
+    cp ${i} ${goalpath}/${i##*/}
+   fi
+  fi
+ done
+done
+
+#backup entired folder to tar
+#tar -zcvf /backup/${timep}/backup.tar.gz /mail/linkgent.com/
+#timed=`date -d today +"%Y-%m-%d_%H:%M:%S"`
+#echo "startTime=${timep} and endTime=${timed}" >> /backup/${timep}/last
