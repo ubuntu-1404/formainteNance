8a742102818bb1f515a85ab8952981214ce903bd
ubuntu-1404 <simul2013@126.com>
2015-12-25 11:32:54 +0800

    Update cut.sh
    add get parameters from inputing

--- cut.sh
+++ cut.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#get runtime parameters from inputing
+if [ $# -eq 0 ] ; then
+        echo "no parameters !"
+        exit
+else
+        for i in "$@" ; do
+                echo "${i}";
+        done
+fi
+
+#make string to be substring one
 tmp0="aa.bb.cc.dd"
 echo "${tmp0}##*.==>${tmp0##*.}"
 echo "${tmp0}%%.*==>${tmp0%%.*}"
